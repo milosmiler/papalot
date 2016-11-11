7482348a833b7fb829e2c7671255ef63168ff8eb
jairo villalobos <jairo.villalobos@lunave.com>
2016-11-11 15:16:30 -0600

    primer commit

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+const gulp = require('gulp'),
+      sass = require('gulp-sass'),
+      pug = require('gulp-pug'),
+      imagemin = require('gulp-imagemin'),
+      autoprefixer = require('gulp-autoprefixer'),
+      browserSync = require('browser-sync').create();
+
+// compilacions de sass a css
+gulp.task('sass', () =>
+    gulp.src('./Components/Scss/*.scss')
+        .pipe(sass({
+            outputStyle: 'expended',
+            sourceComments: false
+        }))
+        .pipe(autoprefixer({
+            version: ['last 2 browsers']
+        }))
+        .pipe(gulp.dest('./dest/css'))
+        .pipe(browserSync.stream())
+    );
+
+// compilacion gulp a html
+gulp.task('pug', () =>
+    gulp.src('./Components/Pug/*.pug')
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./dest'))
+        .on('end', browserSync.reload)
+    );
+
+// optimizar imagenes
+gulp.task('imagemin', () =>
+    gulp.src('./Components/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
+);
+
+
+// servido que mostrara los cambios
+gulp.task('default',() => {
+    browserSync.init({
+        server:'./dest'
+    });
+
+    gulp.watch('Components/Scss/*.scss',['sass']);
+    gulp.watch('Components/Pug/**/*.pug',['pug']);
+
+})
+
+
+
+
+
+
+
+
+
